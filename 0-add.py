60effebb3f9ee6a97c691a8db5bd3b822ddd44f9
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-01 15:55:57 +0200

    adding floating numbers

---
+++ 0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers and return the result
+    
+    Args:
+        a (float): The first float number
+        b (float): The second float number
+        
+    Returns:
+        float: The sum of 'a' and 'b'
+    """
+    return a + b
