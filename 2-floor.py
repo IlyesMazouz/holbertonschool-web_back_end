729210b034787ddaad4e682d178f88bbd12b9290
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-03 13:29:13 +0200

    returning floor

---
+++ 2-floor.py
@@ -0,0 +1,13 @@
+import math
+
+"""
+a type-annotated function floor which takes a float n
+as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the input float
+    """
+    return math.floor(n)
