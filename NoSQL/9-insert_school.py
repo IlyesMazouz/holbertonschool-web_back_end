1933c1664a6ae22ab79cd035823e0b530ee42daa
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-08 10:29:10 +0200

    inserting new docs

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a Python function that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a MongoDB collection based on keyword arguments
+    """
+    inserted_document = kwargs
+    result = mongo_collection.insert_one(inserted_document)
+    return result.inserted_id
