a5585bc857d5d9f32e5c5b3621ff004e51cabd4c
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-08 15:50:23 +0200

    asynchronous function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,32 +1,17 @@
 #!/usr/bin/env python3
 """
-asyncio.Task
+asyncio task function
+
 """
+
 import asyncio
 import random
 
-
-async def wait_random(max_delay: int) -> float:
-    """
-    asynchronous function that waits for a
-    random amount of time between 0 and max_delay
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    create an asyncio.Task that wraps the wait_random function
+    task_wait_random function
     """
     return asyncio.create_task(wait_random(max_delay))
-
-
-async def test(max_delay: int) -> None:
-    task = task_wait_random(max_delay)
-    await task
-    print(task.__class__)
-
-
-asyncio.run(test(5))
