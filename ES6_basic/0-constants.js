23e7185fb9b63ec8688935d5131551714ab93f37
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-19 11:31:28 +0200

    switching variables

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+// switching variables in each function to const and let
+
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
+  
