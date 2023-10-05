6f0f9baf5cb4bdebe9ba9dcc6884e6c379bb9ce7
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-10-05 11:28:29 +0200

    handling sign up

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
-export default async function handleProfileSignup(firstName, lastName, fileName,) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = {
-    status: 'pending',
+    status: 'pending'
   };
   const photoPromise = {
-    status: 'pending',
+    status: 'pending'
   };
   try {
     const userResult = await signUpUser(firstName, lastName);
