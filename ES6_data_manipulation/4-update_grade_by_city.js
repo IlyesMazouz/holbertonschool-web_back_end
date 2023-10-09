cc672b0a6b878a8ecf135c50fe23015f0e4886c3
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-10-09 20:51:29 +0200

    returning students with specific grades/city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+      return {
+        ...student,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
+}
+
+export default updateStudentGradeByCity;
