5bfbbf5656e97b949fadb9343db6aeacdb67512f
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-13 13:15:11 +0100

    storing and printing cache

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,19 +4,18 @@
 
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """ basicCache inherits from BaseCaching and is a caching system without limit
-    """
+    """basicCache inherits from BaseCaching and
+     is a caching system without limit"""
 
     def put(self, key, item):
-        """ add an item in the cache
-        """
+        """add an item in the cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get an item by key
-        """
+        """get an item by key"""
         if key is not None:
             return self.cache_data.get(key)
         return None
