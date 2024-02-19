bf9df079cd000756e04f0ec77178f5898c854b88
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-19 14:34:29 +0100

    filtering data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+A module for filtering sensitive data in log messages
+"""
+
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    Filters sensitive data in a log message by obfuscating specified fields
+    """
     for field in fields:
         message = re.sub(
             r"{}=[^{}]+".format(field, re.escape(separator)),
@@ -11,3 +21,14 @@ def filter_datum(fields, redaction, message, separator):
             message,
         )
     return message
+
+
+if __name__ == "__main__":
+    fields = ["password", "date_of_birth"]
+    messages = [
+        "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
+        "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;",
+    ]
+
+    for message in messages:
+        print(filter_datum(fields, "xxx", message, ";"))
