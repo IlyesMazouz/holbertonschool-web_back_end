ff26b962746ac7efc488caa86aea6009cd97ec4d
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-20 17:20:09 +0100

    confirming hashed password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -17,3 +17,12 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password.
+    """
+    password_bytes = password.encode("utf-8")
+
+    return bcrypt.checkpw(password_bytes, hashed_password)
