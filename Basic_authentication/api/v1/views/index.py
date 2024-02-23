3931e66ca3557a74345a5687027062e11ec1cc46
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-23 13:53:54 +0100

    index.py file

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python3
-""" Module of Index views """
-from flask import jsonify, abort
-from api.v1.views import app_views
+""" Module of Index views
+"""
+from flask import abort, Blueprint
 
+app_views = Blueprint('app_views', __name__)
 
-@app_views.route("/status", methods=["GET"], strict_slashes=False)
-def status() -> str:
-    """GET /api/v1/status"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats/", strict_slashes=False)
-def stats() -> str:
-    """GET /api/v1/stats"""
-    from models.user import User
-
-    stats = {}
-    stats["users"] = User.count()
-    return jsonify(stats)
-
-
-@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
-def unauthorized() -> str:
-    """GET /api/v1/unauthorized"""
-    abort(401)
+@app_views.route('/forbidden', methods=['GET'])
+def forbidden_endpoint():
+    """Handler for forbidden endpoint."""
+    abort(403)
