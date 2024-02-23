c454e6e71ae9f89ac1d0e0e315a0f66bad4f1235
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-23 13:46:53 +0100

    index.py file

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -3,23 +3,24 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    """
+    """GET /api/v1/status"""
     return jsonify({"status": "OK"})
 
-@app_views.route('/stats/', strict_slashes=False)
+
+@app_views.route("/stats/", strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    """
+    """GET /api/v1/stats"""
     from models.user import User
+
     stats = {}
-    stats['users'] = User.count()
+    stats["users"] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    """
+    """GET /api/v1/unauthorized"""
     abort(401)
