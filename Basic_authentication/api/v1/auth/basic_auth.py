61ef85c2cc830efc2d65fe30dea23bbfcb32d88c
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-24 11:44:01 +0100

    basic_auth.py

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module containing the BasicAuth class for basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class for basic authentication"""
+
+    pass
