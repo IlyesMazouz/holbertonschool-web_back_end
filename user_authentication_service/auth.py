3a9989dd3e8de170cf7b0bb5a9f864193b6c263a
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-26 11:50:02 +0100

    creating new user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,17 +1,29 @@
-#!/usr/bin/env python3
-
 """Auth module
 """
+from db import DB
 
-import bcrypt
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """Register a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except:
+            pass
+
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt"""
+    import bcrypt
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
-
-
-if __name__ == "__main__":
-    print(_hash_password("Hello Holberton"))
