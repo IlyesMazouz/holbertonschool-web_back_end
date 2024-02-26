b19934680fea4f0c141546fad20ee60492ef328f
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-26 11:54:12 +0100

    greeting msg

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,10 +6,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """Route handler for the root endpoint."""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
