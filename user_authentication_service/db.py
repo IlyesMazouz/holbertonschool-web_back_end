f222497c3f46870d8d302afe68cf62e95050c9be
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-26 11:38:38 +0100

    find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,9 +6,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
-
 
 class DB:
     """DB class"""
@@ -16,7 +17,6 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
-        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
@@ -34,3 +34,34 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database by given criteria"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
+
+if __name__ == "__main__":
+    my_db = DB()
+
+    user = my_db.add_user("test@test.com", "PwdHashed")
+    print(user.id)
+
+    find_user = my_db.find_user_by(email="test@test.com")
+    print(find_user.id)
+
+    try:
+        find_user = my_db.find_user_by(email="test2@test.com")
+        print(find_user.id)
+    except NoResultFound:
+        print("Not found")
+
+    try:
+        find_user = my_db.find_user_by(no_email="test@test.com")
+        print(find_user.id)
+    except InvalidRequestError:
+        print("Invalid")
