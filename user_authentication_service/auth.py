d6ef13ea862941538d3a7ecbbe348fbc0ecdf538
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-27 16:40:09 +0100

    auth.py file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 """
 
 from db import DB
+import bcrypt
 
 
 class Auth:
@@ -12,23 +13,23 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
-        """Register a new user"""
+    def register_user(self, email: str, password: str) -> None:
+        """Register a new user."""
+        hashed_password = self._hash_password(password)
+        self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the login is valid."""
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode("utf-8"), user.hashed_password.encode("utf-8")
+            )
         except:
-            pass
+            return False
 
-        hashed_password = _hash_password(password)
-        user = self._db.add_user(email, hashed_password)
-        return user
-
-
-def _hash_password(password: str) -> bytes:
-    """Hashes a password using bcrypt"""
-    import bcrypt
-
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-    return hashed_password
+    def _hash_password(self, password: str) -> str:
+        """Hashes a password using bcrypt."""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+        return hashed_password.decode("utf-8")
