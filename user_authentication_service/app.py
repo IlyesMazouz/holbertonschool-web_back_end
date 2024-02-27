d7eea8787978db511eb2bce8740cccb348cad72d
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-27 16:38:15 +0100

    app.py file

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,15 +2,24 @@
 """
 app.py
 """
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
 
+AUTH = Auth()
 
-@app.route("/")
-def index():
-    """Route handler for the root endpoint."""
-    return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Endpoint to register a new user"""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as err:
+        return jsonify({"message": str(err)}), 400
 
 
 if __name__ == "__main__":
