6eaa993a16027c29cd365205bf1d85638ff133c7
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-02-27 16:42:17 +0100

    auth.py file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
-"""Auth module
-"""
+"""Auth module"""
 
 from db import DB
 import bcrypt
-
+import uuid  
 
 class Auth:
-    """Auth class to interact with the authentication database."""
+    """Auth class to interact with the authentication database"""
 
     def __init__(self):
+        """Initialize Auth object with a DB instance"""
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> None:
-        """Register a new user."""
+        """Register a new user"""
         hashed_password = self._hash_password(password)
         self._db.add_user(email, hashed_password)
 
     def valid_login(self, email: str, password: str) -> bool:
-        """Check if the login is valid."""
+        """Check if the login is valid"""
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(
@@ -29,7 +29,11 @@ class Auth:
             return False
 
     def _hash_password(self, password: str) -> str:
-        """Hashes a password using bcrypt."""
+        """Hashes a password using bcrypt"""
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
         return hashed_password.decode("utf-8")
+
+    def _generate_uuid(self) -> str:
+        """Generate a new UUID"""
+        return str(uuid.uuid4())  
