21c1e155978ae10ad75b83311d8effffcf33457c
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-08-11 19:42:06 +0200

    system count

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 """
-this module defines a Cache class for storing and retrieving data in Redis.
+this module defines a Cache class for storing and retrieving data in Redis
+It also includes a decorator to count the number of times a method is called
 """
 
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -15,6 +28,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
 
         key = str(uuid.uuid4())
@@ -22,7 +36,8 @@ class Cache:
         return key
 
     def get(
-        self, key: str, fn: Optional[Callable[[bytes], Union[str, int, bytes]]] = None
+        self, key: str,
+        fn: Optional[Callable[[bytes], Union[str, int, bytes]]] = None
     ) -> Union[str, int, bytes, None]:
 
         value = self._redis.get(key)
