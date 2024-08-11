b4ab0ec28f8b6218cebc78d4e76b7cc70eeac8bc
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-08-11 20:10:41 +0200

    replay

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -71,3 +71,19 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
 
         return self.get(key, lambda d: int(d))
+
+
+def replay(method: Callable):
+
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+
+    for input_data, output_data in zip(inputs, outputs):
+        print(
+            f"{method.__qualname__}(*{input_data.decode('utf-8')}) -> {output_data.decode('utf-8')}"
+        )
