1a3e47e02a0f0884a8fb290f34d83aa401f9c7a3
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-08-11 19:35:37 +0200

    type of data

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 """
-this module defines a Cache class for storing data in Redis
+this module defines a Cache class for storing and retrieving data in Redis.
 """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
-    """
-    cache class that provides a simple interface for storing and retrieving data
-    from a Redis database
-    """
 
     def __init__(self):
-        """
-        initialize the Cache instance with a Redis client and flush the database
-        """
+
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        store data in Redis with a randomly generated key
-        """
+
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable[[bytes], Union[str, int, bytes]]] = None
+    ) -> Union[str, int, bytes, None]:
+
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+
+        return self.get(key, lambda d: int(d))
