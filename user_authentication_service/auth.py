46a5d017beb8fb2666805a71c7f574a72f060301
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-04 16:33:03 +0100

    auth.py file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,40 +1,18 @@
 #!/usr/bin/env python3
+"""
+Module for hashing passwords.
 
-"""Auth module"""
-
-from db import DB
+"""
 import bcrypt
-import uuid
 
 
-class Auth:
-    """Auth class to interact with the authentication database"""
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password string using bcrypt with a salt.
 
-    def __init__(self):
-        """Initialize Auth object with a DB instance"""
-        self._db = DB()
+    """
+    salt = bcrypt.gensalt()
 
-    def register_user(self, email: str, password: str) -> None:
-        """Register a new user"""
-        hashed_password = self._hash_password(password)
-        self._db.add_user(email, hashed_password)
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
-    def valid_login(self, email: str, password: str) -> bool:
-        """Check if the login is valid"""
-        try:
-            user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(
-                password.encode("utf-8"), user.hashed_password.encode("utf-8")
-            )
-        except:
-            return False
-
-    def _hash_password(self, password: str) -> str:
-        """Hashes a password using bcrypt"""
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-        return hashed_password.decode("utf-8")
-
-    def _generate_uuid(self) -> str:
-        """Generate a new UUID"""
-        return str(uuid.uuid4())
+    return hashed_password
