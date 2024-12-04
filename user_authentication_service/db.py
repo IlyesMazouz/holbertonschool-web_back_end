7f9493551f607115772cebc8888b8595e9a1b844
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-04 12:35:37 +0100

    db.py file

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
-"""DB module
-"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
-from user import Base, User
+from user import User, Base
 
 
 class DB:
@@ -18,6 +13,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
@@ -30,47 +26,11 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        """Adds a new user to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        self._session.add(new_user)
+
         self._session.commit()
-        return user
 
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database by given criteria"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound
-            return user
-        except InvalidRequestError:
-            raise
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a user's attributes"""
-        try:
-            user = self.find_user_by(id=user_id)
-            for key, value in kwargs.items():
-                if hasattr(user, key):
-                    setattr(user, key, value)
-                else:
-                    raise ValueError(f"Attribute '{key}' is not a valid user attribute")
-            self._session.commit()
-        except NoResultFound:
-            raise
-
-
-if __name__ == "__main__":
-    my_db = DB()
-
-    email = "test@test.com"
-    hashed_password = "hashedPwd"
-
-    user = my_db.add_user(email, hashed_password)
-    print(user.id)
-
-    try:
-        my_db.update_user(user.id, hashed_password="NewPwd")
-        print("Password updated")
-    except ValueError:
-        print("Error")
+        return new_user
