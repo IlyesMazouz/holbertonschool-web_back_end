774b308db4a68790c10bc5b772a090ada025f60b
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-04 11:54:45 +0100

    basic_auth.py file

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,31 +1,10 @@
 #!/usr/bin/env python3
 """
-Authentication module for the API
+BasicAuth module that inherits from Auth
 """
-from flask import request
-from typing import List, TypeVar
+from api.v1.auth.auth import Auth
 
 
-class Auth:
-    """Auth class to manage the API authentication."""
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication."""
-        if path is None or excluded_paths is None:
-            return True
-
-        for excluded_path in excluded_paths:
-            if path == excluded_path or path.startswith(excluded_path.rstrip('/')):
-                return False
-
-        return True
-
-    def authorization_header(self, request=None) -> str:
-        """Gets the Authorization header from the request."""
-        if request is None:
-            return None
-        return request.headers.get("Authorization")
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Gets the current user (to be implemented in child classes)."""
-        return None
+class BasicAuth(Auth):
+    """BasicAuth class for handling basic authentication"""
+    pass
