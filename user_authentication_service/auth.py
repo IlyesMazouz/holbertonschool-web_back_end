dda4e8e86332bd6dbb703f6da43a384124adc8f3
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-04 17:22:43 +0100

    auth.py

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -11,7 +11,7 @@ import bcrypt
 class Auth:
     def __init__(self):
         """Initializes the Auth class."""
-        pass
+        self._db = {}
 
     def _generate_uuid(self) -> str:
         """
@@ -22,10 +22,15 @@ class Auth:
     def register_user(self, email: str, password: str) -> str:
         """
         Registers a new user by email and password.
-
         """
-        user_id = self._generate_uuid() 
+        user_id = self._generate_uuid()
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        self._db[email] = {
+            "user_id": user_id,
+            "password": hashed_password,
+            "session_id": None,
+        }
 
         return user_id
 
@@ -33,6 +38,21 @@ class Auth:
         """
         Validates the login credentials for a user.
         """
-        stored_password_hash = b"$2b$12$somehashedpasswordhere"
+        if email not in self._db:
+            return False
 
+        stored_password_hash = self._db[email]["password"]
         return bcrypt.checkpw(password.encode("utf-8"), stored_password_hash)
+
+    def create_session(self, email: str) -> str:
+        """
+        Creates a session for the user by generating a new session ID.
+        """
+        if email not in self._db:
+            return None
+
+        session_id = self._generate_uuid()
+
+        self._db[email]["session_id"] = session_id
+
+        return session_id
