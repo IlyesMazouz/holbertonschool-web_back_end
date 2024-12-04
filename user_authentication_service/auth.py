4618b579cebcdcb26b079ca95755fc1c9af5c181
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-04 16:40:35 +0100

    auth.py file

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 """
-Module for hashing passwords.
-
+Module for handling user authentication
 """
+
+from db import DB
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Optional
 
 
-def _hash_password(password: str) -> bytes:
-    """
-    Hash a password string using bcrypt with a salt.
+class Auth:
+    """Auth class to interact with the authentication database."""
 
-    """
-    salt = bcrypt.gensalt()
+    def __init__(self):
+        self._db = DB()
 
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    def _hash_password(self, password: str) -> bytes:
+        """Hash a password using bcrypt."""
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode("utf-8"), salt)
 
-    return hashed_password
+    def register_user(self, email: str, password: str) -> Optional[object]:
+        """Register a new user with email and password."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_password = self._hash_password(password)
+
+        user = self._db.add_user(email, hashed_password.decode("utf-8"))
+        return user
