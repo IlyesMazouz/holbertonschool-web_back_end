d974bbea56853844f7fc391d5f3aa5df5ee8be66
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-06 21:27:49 +0100

    fix

--- MySQL_Advanced/0-list_databases.sql
+++ MySQL_Advanced/0-list_databases.sql
@@ -1,8 +1,8 @@
---  SQL script that creates a table users
---  If the table already exists, your script should not fail
+-- Create the users table if it doesn't exist
+-- The table includes an id as primary key, email as unique, and name as a string
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
 );
