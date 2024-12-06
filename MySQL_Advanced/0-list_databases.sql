39c69507ebef22f324424f5321e313ad90890693
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-06 21:19:03 +0100

    0-list_databases.sql file

--- MySQL_Advanced/0-list_databases.sql
+++ MySQL_Advanced/0-list_databases.sql
@@ -1,12 +1,7 @@
--- This script creates the 'users' table if it does not already exist
--- It ensures that the 'email' column is unique to avoid duplicate entries
-
-CREATE TABLE
-IF NOT EXISTS users
-(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR
-(255) NOT NULL UNIQUE,
-    name VARCHAR
-(255)
+-- Create the users table if it doesn't exist
+-- The table includes an id as primary key, email as unique, and name as a string
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,  -- id is an auto incrementing primary key
+  email VARCHAR(255) NOT NULL UNIQUE,  -- email is unique and not null
+  name VARCHAR(255)  -- name can be null
 );
