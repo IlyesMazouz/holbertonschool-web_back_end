b082f56bd5c6a6c9db9c6adc181f3ae65f084f46
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-06 21:24:07 +0100

    0-list_databases.sql file

--- MySQL_Advanced/0-list_databases.sql
+++ MySQL_Advanced/0-list_databases.sql
@@ -1,7 +1,8 @@
 -- Create the users table if it doesn't exist
 -- The table includes an id as primary key, email as unique, and name as a string
+
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,  -- id is an auto incrementing primary key
-  email VARCHAR(255) NOT NULL UNIQUE,  -- email is unique and not null
-  name VARCHAR(255)  -- name can be null
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
 );
