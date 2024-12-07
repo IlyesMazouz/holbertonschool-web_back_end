b6db8d1a280b4aaaba00683f4d03c74ba4e5e2b0
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-12-07 11:11:43 +0100

    11-need_meeting.sql file

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- View need_meeting: Lists students with a score under 80 and no recent meeting.
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
