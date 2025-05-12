22f83d0442255f193ccf148caa1e81f12764f1e8
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-05-12 18:37:06 +0200

    handling logout request

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -37,3 +37,14 @@ def session_login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
+def session_logout():
+    """Handles session logout for a user"""
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
