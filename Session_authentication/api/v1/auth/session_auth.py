733203e87338fc69b5056228da604584061596fb
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-05-12 13:49:45 +0200

    updting session auth file

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,23 @@
 Session Authentication Class
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id and stores
+        it in the user_id_by_session_id dictionary"""
+
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
