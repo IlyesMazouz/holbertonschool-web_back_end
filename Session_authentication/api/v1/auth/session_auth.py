d077a52b9eb9acf4ec785c9558fc3e79ea79ea1a
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-05-12 17:09:42 +0200

    updating session auth file

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,24 +1,50 @@
 #!/usr/bin/env python3
-"""Session authentication class"""
-from uuid import uuid4
+"""
+SessionAuth module for managing session-based authentication
+"""
 from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
-    """Session Authentication"""
+    """SessionAuth class for managing session authentication"""
 
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a Session ID for a user_id"""
+        """Creates a session ID for a user ID"""
         if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = str(uuid4())
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID"""
+        """Retrieves a user ID based on a session ID"""
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def session_cookie(self, request=None):
+        """Retrieves the session cookie value from a request"""
+        if request is None:
+            return None
+        from os import getenv
+
+        session_name = getenv("SESSION_NAME")
+        if session_name is None:
+            return None
+        return request.cookies.get(session_name)
+
+    def current_user(self, request=None):
+        """Retrieves a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
